6a622070a388cfd1b950244da28854eb0cab470a
Austin <austinpulley19@yahoo.com>
2022-02-07 14:38:37 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(cards) {
+    const giftCards = []
+    console.log(cards)
+    for (let i=0; i<cards.length; i++) {
+        giftCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return giftCards;
+}
+function countDown (){
+    var countdown = 11;
+    while (countdown > 0){
+        countdown--;
+        console.log(countdown);
+    }
+}
+   
